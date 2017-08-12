acb5465896d4743605e71433f38110db3c5ede26
Austin Gill <notgnoshi@gmail.com>
2017-08-12 12:39:33 -0500

    Disable clean.sh
    * I must just be stupid...

--- bin/clean.sh
+++ bin/clean.sh
@@ -15,65 +15,64 @@ usage() {
 
 strip() {
 
-	echo "Stripping trailing whitespace."
-		sed -i '' 's/[ \t]*$//' "($2)"
+	# echo "Stripping trailing whitespace."
+	# sed -i '' 's/[ \t]*$//' "($2)"
 
-		echo "Converting tabs to spaces."
-		expand --tabs="$1" "$2"
+	# echo "Converting tabs to spaces."
+	# expand --tabs="$1" "$2"
 
-		echo "Ensuring only one newline at end of file"
-		awk '/^$/ {nlstack=nlstack "\n";next;} {printf "%s",nlstack; nlstack=""; print;}' "$2"
+	# echo "Ensuring only one newline at end of file"
+	# awk '/^$/ {nlstack=nlstack "\n";next;} {printf "%s",nlstack; nlstack=""; print;}' "$2"
 }
 
 main() {
-	NUM=4
-		while getopts ":n:r:h" opt; 
+	NUM=3
+    while getopts ":n:r:h" opt; 
 	do
-		case $opt in
-# Set NumSpaces
+        case $opt in
+            # Set NumSpaces
 			n)
-			if [ "$OPTARG" -eq "$OPTARG" ]; then
-				NUM=$OPTARG
-			else
-				usage
-					fi
-					;;
-# Recursive
-	r)
-		if [ -d $file ]; then
-			for file in $( find $OPTARG -type f ); do
-				echo $file
-					strip NUM $file
+			    if [ "$OPTARG" -eq "$OPTARG" ]; then
+				    NUM=$OPTARG
+			    else
+				    usage
+			    fi
+			    ;;
+            # Recursive
+            r)
+		        if [ -d $file ]; then
+			        for file in $( find $OPTARG -type f ); do
+				        echo $file
+					    strip NUM $file
 					done
-		else
-			usage
+		        else
+			        usage
 				fi
 				return
 				;;
-# Error states
-	\?)
-		echo Invalid Option
-		;&
-		:)
-		usage
-		;&
-		\h)
-		usage
-		;;
-	esac
-		done
-		shift $((OPTIND -1))
+            # Error states
+	        \?)
+		        echo Invalid Option
+		        ;&
+		    :)
+		        usage
+		        ;&
+		    \h)
+		        usage
+		        ;;
+    	esac
+    done
+	shift $((OPTIND -1))
 
-		if [ $# -lt 2 ]; then
-			usage
-				exit 1
-		else
-			for file in $@; do 
-				strip NUM $file
-					done
-					fi
-
+	if [ $# -lt 2 ]; then
+		usage
+		exit 1
+	else
+		for file in $@; do 
+			strip NUM $file
+        done
+	fi
 }
 
-    main "$@"
+main "$@"
 
