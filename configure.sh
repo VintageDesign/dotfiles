5681d9df296254732af2c34e4c945c7dca0067e8
Austin Gill <notgnoshi@gmail.com>
2017-10-23 10:46:29 -0600

    Add TODO notes

--- configure.sh
+++ configure.sh
@@ -9,7 +9,7 @@ read -p "Install git, vim, and unp? (y/N) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "Installing Essentials..."
-    sudo apt install git vim unp
+    sudo apt install git vim unp htop
 fi
 
 # Clone dotfiles repository if script is being ran from a flash drive
@@ -40,11 +40,12 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 	cd -
 fi
 
-# Install fzf
+# Install fzf, requires the fzf submodule to be initialized and updated
 read -p "Install fzf? (y/N) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "Installing fzf"
+    # TODO: pipe `yes` into install script?
     $HOME/.fzf/install
     echo "Done installing fzf"
 fi
@@ -121,6 +122,7 @@ read -p "Install Atom and Java? (y/N) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "Installing Atom and Java..."
+    # TODO: make minecraft work nicely with java9
     sudo apt install atom oracle-java8-installer
 fi
 
@@ -129,7 +131,8 @@ read -p "Install LaTeX and related? (y/N) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "Installing texlive, chktex..."
-    sudo apt install texlive-full chktex pdf2svg pandoc
+    # TODO: Don't install texmaker from repository -- out of date?
+    sudo apt install texlive-full chktex pdf2svg pandoc texmaker
 fi
 
 # Install dev packages
@@ -137,13 +140,13 @@ read -p "Install dev packages? (y/N) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "Installing dev packages..."
-    sudo apt install gcc g++ make clang shellcheck gdb pep8 libcppunit-dev astyle doxygen python3-setuptools
+    sudo apt install gcc g++ make clang shellcheck gdb pep8 libcppunit-dev astyle doxygen python3-setuptools pv
     sudo easy_install3 pip
-    sudo -H pip install --upgrade pip
+    sudo -H pip install --upgrade pip pylint pycodestyle
 fi
 
 # Install useful packages
-read -p "Install headless packages? (y/N) " -n 1 -r
+read -p "Install useful utilities? (y/N) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
 	echo "Installing headless Packages...."
@@ -154,7 +157,8 @@ fi
 read -p "Install GUI packages? (y/N) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    sudo apt install pithos texmaker printer-driver-escpr
+    # printer-driver-escpr isn't a GUI package, but you only use it on a non-headless system
+    sudo apt install pithos printer-driver-escpr gnome-tweak-tool
 fi
 
 # Install useful Python 3 packages
@@ -162,7 +166,6 @@ read -p "Install python3 scipy stack? (y/N) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
 	echo "Installing Python Packages..."
-    sudo -H pip install --upgrade pycodestyle pylint
     sudo -H pip install --upgrade pygments ipython matplotlib sympy scipy numpy networkx nmap pandas seaborn
 fi
 
@@ -171,7 +174,6 @@ read -p "Install Jupyter? (y/N) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
 	echo "Installing Jupyter..."
-	sudo -H pip install --upgrade pip
 	sudo -H pip install --upgrade jupyter ipython
 fi
 
@@ -188,7 +190,9 @@ fi
 read -p "Install Atom packages? (y/N) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	apm install linter linter-chktex linter-gcc linter-pep8 linter-shellcheck intentions busy-signal linter-ui-default autoclose-html autocomplete-python highlight-selected language-latex language-liquid language-viml language-haskell markdown-preview-plus minimap minimap-cursorline minimap-find-and-replace minimap-git-diff minimap-highlight-selected tabs-to-spaces autocomplete-clang file-icons file-watcher highlight-selected gruvbox-plus-syntax pdf-view latex latexer python-autopep8 language-matlab
+    # TODO: There are some redundant linters. Pick which one(s) to use. (clang vs gcc, pycodestyle vs pylint vs pep8 vs flake8)
+    # TODO: decide if file-watcher is still necessary with the new atom update that provides a file watcher API
+	apm install linter linter-chktex linter-gcc linter-pep8 linter-shellcheck intentions busy-signal linter-ui-default autoclose-html autocomplete-python highlight-selected language-latex language-liquid language-viml language-haskell markdown-preview-plus minimap minimap-cursorline minimap-find-and-replace minimap-git-diff minimap-highlight-selected tabs-to-spaces autocomplete-clang file-icons file-watcher highlight-selected gruvbox-plus-syntax pdf-view latex latexer python-autopep8 language-matlab linter-pylint
 fi
 
 # Update and upgrade system
@@ -200,9 +204,24 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 	sudo apt autoremove && sudo apt autoclean
 fi
 
-# Install fonts
-# TODO: Find way to store font files
+# TODO: setup [s]ftp server on limbo to serve assets?
+#       * what to do when updating/configuring limbo?
+
+# TODO: configure fonts
 # sudo cp assets/Fonts/Meslo/MesloLGSDZ-Regular.ttf /usr/share/fonts/
 # sudo cp assets/Fonts/Apple\ San\ Fransisco/SystemSanFranciscoDisplayBold.ttf assets/Fonts/Apple\ San\ Fransisco/SystemSanFranciscoDisplayRegular.ttf /usr/share/fonts/
 #
 # sudo fc-cache -fv
+
+# TODO: setup minecraft.desktop and startup script(s)
+
+# TODO: setup terminal profile(s)?
+
+# TODO: setup system preferences
+
+# TODO: setup /etc/hosts
+
+# TODO: setup ~/.ssh/config
+
+# TODO: setup SDSMT VPN
+
