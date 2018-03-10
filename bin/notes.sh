a898b84b4840f74f4fc0fbc2d5bf6b6c0e5a8844
Austin Gill <notgnoshi@gmail.com>
2018-03-10 14:11:01 -0700

    improve note taking script. Add search functionality

--- bin/notes.sh
+++ bin/notes.sh
@@ -1,24 +1,50 @@
 #!/bin/bash
 set -e
-
 notes_path="${HOME}/Documents/notes"
-filename="${notes_path}/$(date +%y-%m-%d).md"
-touch "${filename}"
-# Make read/writeable only by user
-chmod 600 "${filename}"
 
-today="$(date '+%A %B %d at %I:%M%P')"
-if [[ -s ${filename} ]];
-then
-    # File is not empty, add newline before and after the new heading
-    {
-        echo ""
-        echo "# ${today}"
-        echo ""
-    } >> "${filename}"
+if [ $# -gt 0 ]; then
+    while getopts ":s:h" opt; do
+        case "$opt" in
+            s )
+                grep -ir --color=auto "$OPTARG" "$notes_path"
+                ;;
+            \? )
+                echo "Invalid options: -$OPTARG" >&2
+                exit 1
+                ;;
+            : )
+                echo "Option -$OPTARG requires a regex or quoted text argument" >&2
+                exit 1
+                ;;
+            h )
+                echo "Usage:"
+                echo "    $0 -h                       Display this help message"
+                echo "    $0 -s PATTERN               Searches $notes_path for PATTERN"
+                echo "    $0                          Opens today's notes file for editing"
+                ;;
+        esac
+    done
 else
-    # File is empty, add the first heading
-    echo "# ${today}" >> "${filename}"
+    # Today's filename
+    filename="${notes_path}/$(date +%y-%m-%d).md"
+    touch "${filename}"
+    # Make read/writeable only by user
+    chmod 600 "${filename}"
+
+    today="$(date '+%A %B %d at %I:%M%P')"
+    if [[ -s ${filename} ]];
+    then
+        # File is not empty, add newline before and after the new heading
+        {
+            echo ""
+            echo "# ${today}"
+            echo ""
+        } >> "${filename}"
+    else
+        # File is empty, add the first heading
+        echo "# ${today}" >> "${filename}"
+    fi
+
+    vim "${filename}"
 fi
 
-vim "${filename}"
