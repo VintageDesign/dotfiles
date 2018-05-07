b81da82645ea38abff60452496b4adcd71f41103
Austin Gill <notgnoshi@gmail.com>
2018-05-06 19:06:15 -0600

    add disapproval script

---
+++ bin/disapprove.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import argparse
+
+
+def parse_args():
+    """
+        Disapprovingly parses arguments.
+    """
+    DESCRIPTION = 'Disapproves with ever-increasing fervor.'
+    VERSION = '0.2'
+
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+
+    parser.add_argument('--version', action='version', version=VERSION)
+    parser.add_argument('-v', '--verbose', action='count', help='Increase verbosity e.g.: -vvvv')
+
+    return parser.parse_args()
+
+def main(args):
+    """
+        Disapproves with ever-increasing fervor.
+    """
+    if args.verbose is None:
+        print(u'ಠ_ಠ')
+    elif args.verbose == 1:
+        print(u'ಠ益ಠ')
+    elif args.verbose == 2:
+        print(u'( ͡ಠ ʖ̯ ͡ಠ)')
+    elif args.verbose == 3:
+        print(u'(ノಠ益ಠ)ノ')
+    elif args.verbose == 4:
+        print(u'(╯°□°）╯︵ ┻━┻')
+    else:
+        print(u'(ノಠ益ಠ)ノ彡┻━┻')
+
+
+
+if __name__ == '__main__':
+    main(parse_args())
