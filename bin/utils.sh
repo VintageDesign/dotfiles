dce74dd64c85b7b2cb3ab0da4f818b738204f582
Austin Gill <austin.gill@sencore.com>
2018-07-09 11:19:13 -0500

    Add interactive git log.
    TODO: Find a way to use as a git alias?

--- bin/utils.sh
+++ bin/utils.sh
@@ -102,3 +102,15 @@ unix2dos()
     # requires GNU sed
     sed -i 's/$/\r/' "$1"
 }
+
+# Browse git log
+glog() {
+    git log --graph --color=always \
+        --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+    fzf --ansi --no-sort --reverse --preview  "echo {} | grep -o '[a-f0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show --color=always %'" \
+        --bind "enter:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}
+FZF-EOF"
+}
