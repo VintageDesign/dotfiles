a6d5c126db8036fe0858e49e80e043a63276e526
Austin Gill <notgnoshi@gmail.com>
2018-10-06 10:47:32 -0600

    Update notes script with relative dates

--- bin/notes.py
+++ bin/notes.py
@@ -5,37 +5,28 @@ from datetime import datetime
 from pathlib import Path
 from subprocess import call
 
+import parsedatetime
+
 
 def parse_args():
     """
         Defines and parses commandline arguments for note-taking script.
     """
     DESCRIPTION = "A small script to facilitate taking, searching, and organizing notes."
-    VERSION = "0.1"
+    VERSION = "0.2"
 
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     group = parser.add_mutually_exclusive_group()
 
     parser.add_argument('--version', action='version', version=VERSION)
-    group.add_argument('-s', '--search',
-                       help='A quoted string regular expression to search for.',
-                       type=str)
     group.add_argument('--todo',
                        action='store_true',
                        default=False,
                        help='Open the TODO list.')
-    group.add_argument('--month',
-                       action='store_true',
-                       default=False,
-                       help='Open this month\'s TODO list.')
-    group.add_argument('--week',
-                       action='store_true',
-                       default=False,
-                       help='Open this week\'s TODO list.')
-    group.add_argument('--day',
-                       action='store_true',
-                       default=False,
-                       help='Open today\'s TODO list.')
+    group.add_argument('-r', '--relative',
+                       nargs='+',
+                       help='Open the note from a relative time. E.g. "yesterday" or "3 days ago"')
+
     # TODO: Add argument to sync notes with server?
     # parser.add_argument('--sync', action='store_true', help='Sync notes with master copies')
 
@@ -57,30 +48,33 @@ def main():
 
     if args.todo:
         FILENAME = 'todo.md'
-    elif args.month:
-        FILENAME = 'month.md'
-    elif args.week:
-        FILENAME = 'week.md'
-    elif args.day:
-        FILENAME = 'day.md'
+    elif args.relative:
+        args.relative = ' '.join(args.relative)
+        cal = parsedatetime.Calendar()
+        time, status = cal.parse(args.relative)
+
+        if not status:
+            print('Failed to parse relative time:', args.relative)
+            exit(1)
+
+        FILENAME = f'{datetime(*time[:6]).strftime("%Y-%m-%d")}.md'
     else:
         # E.g., '2018-05-04.md'.
         FILENAME = f'{datetime.now().strftime("%Y-%m-%d")}.md'
 
-    # Search for the provided pattern and pass and output to stderr or stdout along.
-    if args.search:
-        call(['grep', '-ir', '--color=auto', args.search, NOTES_PATH])
-    # Or just open the given file after appending a nice header to it.
-    else:
-        note = Path(NOTES_PATH + FILENAME)
-        # Append the markdown header to file. Also creates the file if it doesn't exist.
-        with open(note, 'a') as f:
-            f.write(HEADER)
+    path = Path(NOTES_PATH)
+    if not path.exists():
+        path.mkdir(parents=True)
 
-        # Ensure the file is read/writeable only by user, but only after the file is saved.
-        os.chmod(note, 0o600)
-        # Finally, open the file with the editor.
-        call([EDITOR, note])
+    note = Path(NOTES_PATH + FILENAME)
+    # Append the markdown header to file. Also creates the file if it doesn't exist.
+    with open(note, 'a') as f:
+        f.write(HEADER)
+
+    # Ensure the file is read/writeable only by user, but only after the file is saved.
+    os.chmod(note, 0o600)
+    # Finally, open the file with the editor.
+    call([EDITOR, note])
 
 
 if __name__ == '__main__':
