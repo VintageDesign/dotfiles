c8709a6db53f325addfaf5b57382fb2d8202d9ea
Austin Gill <notgnoshi@gmail.com>
2019-01-01 18:01:52 -0700

    Clean up NAS and utility scripts

--- bin/nas.sh
+++ bin/nas.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-echo "Mounting all local NFS shares to /mnt/"
+echo "Toggling NFS mountpoints on local network..."
 
 declare -a mountpoints=("/mnt/assets"
                         "/mnt/documents"
                         "/mnt/images"
                         "/mnt/plex"
-                        "/mnt/projects"
-                        "/mnt/test")
+                        "/mnt/projects")
 
 # Configuration for NFS shares in /etc/fstab.
 # Note this will only work if this computer is on the same local network!
