4c21ca3ec3567b6df200b9c80e8e96bb74481754
Austin Gill <notgnoshi@gmail.com>
2020-02-09 18:27:59 -0700

    Improve man alias to add bash builtins

--- lib/aliases.sh
+++ lib/aliases.sh
@@ -26,15 +26,29 @@ alias today='vim -c VimwikiMakeDiaryNote'
 alias tomorrow='vim -c VimwikiMakeTomorrowDiaryNote'
 alias yesterday='vim -c VimwikiMakeYesterdayDiaryNote'
 
-# Enable colored man pages
-man() {
-    env \
-        LESS_TERMCAP_mb="$(printf "\e[1;31m")" \
-        LESS_TERMCAP_md="$(printf "\e[1;31m")" \
-        LESS_TERMCAP_me="$(printf "\e[0m")" \
-        LESS_TERMCAP_se="$(printf "\e[0m")" \
-        LESS_TERMCAP_so="$(printf "\e[1;44;33m")" \
-        LESS_TERMCAP_ue="$(printf "\e[0m")" \
-        LESS_TERMCAP_us="$(printf "\e[1;32m")" \
-        man "$@"
+function man() {
+    # Export the variables in a subshell to avoid polluting
+    # the global environment in unexpected ways.
+    (
+        export LESS_TERMCAP_mb="$(printf "\e[1;31m")"
+        export LESS_TERMCAP_md="$(printf "\e[1;31m")"
+        export LESS_TERMCAP_me="$(printf "\e[0m")"
+        export LESS_TERMCAP_se="$(printf "\e[0m")"
+        export LESS_TERMCAP_so="$(printf "\e[1;44;33m")"
+        export LESS_TERMCAP_ue="$(printf "\e[0m")"
+        export LESS_TERMCAP_us="$(printf "\e[1;32m")"
+
+        case "$(type -t -- "$1")" in
+        # Use `help -m` to generate a manpage for any bash builtins or keywords
+        builtin | keyword)
+            # Unfortunately the generated manpages are missing the metadata
+            # needed for less to highlight using the LESS_TERMCAP variables above.
+            help -m "$1" | less
+            ;;
+        # Use the system manpages for everything else
+        *)
+            command man "$@"
+            ;;
+        esac
+    )
 }
