d2ca8b9de341b1ca7ea2f0b5532aa064272e7195
Austin Gill <Notgnoshi@gmail.com>
2021-05-01 13:36:53 -0500

    bash: Save history after each command
    This allows sharing history across concurrent sessions.

--- lib/04-history.sh
+++ lib/04-history.sh
@@ -9,4 +9,14 @@ HISTSIZE=-1
 # Save all history
 HISTFILESIZE=-1
 HISTTIMEFORMAT="%h %d %H:%M:%S "
+# Append (not overwrite) to the HISTFILE on session exit.
 shopt -s histappend
+
+# Doesn't matter when using FZF, but useful for vanilla readline
+shopt -s histreedit
+shopt -s histverify
+
+# After each command, append, clear, and reload history.
+# This allows sharing history between concurrent sessions.
+# https://unix.stackexchange.com/questions/1288/preserve-bash-history-in-multiple-terminal-windows
+PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r;"
