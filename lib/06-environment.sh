7b916857d7d7604521aa1e62e3a9447c7381a3a6
Austin Gill <Notgnoshi@gmail.com>
2021-08-22 15:23:16 -0500

    git: Enable side-by-side diffs if the terminal is wide enough
    There are some caveats
    
    * The PROMPT_COMMAND has to run after resizing the terminal
    * side-by-side diffs are always disabled for the git-gl preview window
    * There's an awesome delta PR in progress that enables line-wrapping
      (#515, not linking directly because I don't want it to link to this
      commit)
    * There's an open delta ticket about enabling side-by-side based on the
      column width that sounds perfect, but no work has been done that I can
      tell (#359)
    
      Perhaps git-gl could be rewritten to take advantage of
      __delta_side_by_side_width?
    * If more delta features are enabled in the .gitconfig, they will have
      to be manually added to the delta --features='...' in git-gl
    * LESS=X was removed because it wasn't redrawing the screen and leaving
      old diffs visible.
    
    Closes #74

--- lib/06-environment.sh
+++ lib/06-environment.sh
@@ -62,3 +62,15 @@ export LESS=FRX
 
 # Use parallel make by default
 export MAKEFLAGS="-j$(nproc)"
+
+__delta_side_by_side_width() {
+    local columns=$(tput cols)
+    # Enough room for two side-by-side 80-char diffs with some room for line numbers to spare
+    if [ $columns -ge 170 ]; then
+        export DELTA_FEATURES="side-by-side"
+    else
+        export DELTA_FEATURES=""
+    fi
+}
+
+PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}__delta_side_by_side_width"
