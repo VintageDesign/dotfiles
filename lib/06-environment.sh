bb5d7142d4cbe0f7dc07f541db800070e5b65aa8
Austin Gill <Notgnoshi@gmail.com>
2021-09-18 19:05:04 -0500

    Move Bash completion out of bashrc

--- lib/06-environment.sh
+++ lib/06-environment.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
 ##################################################################################################
 # Environment variables
 ##################################################################################################
---
+++ lib/07-completion.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if ! shopt -oq posix; then
+    if [ -f /usr/share/bash-completion/bash_completion ]; then
+        . /usr/share/bash-completion/bash_completion
+    elif [ -f /etc/bash_completion ]; then
+        . /etc/bash_completion
+    fi
+    if [ ! -d ~/.bash_completion.d ]; then
+        mkdir -p ~/.bash_completion.d
+    fi
+    for completion_file in ~/.bash_completion.d/*; do
+        [ -f "$completion_file" ] && source "$completion_file"
+    done
+    # Find and source clang bash completion scripts
+    for f in $(find /usr/lib -name 'bash-autocomplete.sh'); do
+        source "$f"
+    done
+fi
