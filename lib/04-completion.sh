5ccac87da17da1432b18b3b1ced6fabc542c1a4d
Austin Gill <Notgnoshi@gmail.com>
2022-04-11 07:34:02 -0500

    Add docker-cwd script to run container in CWD
    A shim around docker-run that mounts the CWD in the given container.
    
    TODO: Find a way to make Docker invoke this script with "docker cwd",
    instead of "docker-cwd" (low importance)
    TODO: Add the innumerable other "docker run" arguments to the completion

--- lib/04-completion.sh
+++ lib/04-completion.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Taken from https://stackoverflow.com/questions/47781631/how-to-auto-complete-a-multi-level-command-aliased-to-a-single-command
+# and modified to pass the --repo --tag --id arguments to __docker_complete_images, because
+# otherwise the completion results would include stuff like the image size...
+_docker_cwd() {
+    local previous_extglob_setting
+    previous_extglob_setting=$(shopt -p extglob)
+    shopt -s extglob
+
+    _get_comp_words_by_ref -n : cur prev words cword
+
+    # TODO: Include other "docker run" arguments
+    # Unfortunately /usr/share/bash-completion/completions/docker is a bit hard to follow, so it's
+    # not entirely obvious how to do this.
+    declare -F __docker_complete_images >/dev/null && __docker_complete_images --repo --tag --id
+
+    eval "$previous_extglob_setting"
+    return 0
+}
+
 if ! shopt -oq posix; then
     if [ -f /usr/share/bash-completion/bash_completion ]; then
         . /usr/share/bash-completion/bash_completion
@@ -16,4 +35,9 @@ if ! shopt -oq posix; then
     # for f in $(find /usr/lib -name 'bash-autocomplete.sh'); do
     #     source "$f"
     # done
+
+    if [ -f /usr/share/bash-completion/completions/docker ]; then
+        source /usr/share/bash-completion/completions/docker
+        complete -F _docker_cwd docker-cwd
+    fi
 fi
