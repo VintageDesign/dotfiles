d1c749b6e1f82965f5563928817307ca09979d41
Austin Gill <Notgnoshi@gmail.com>
2022-04-16 21:22:28 -0500

    bash: Add fman helper to search for man pages

--- lib/02-utils.sh
+++ lib/02-utils.sh
@@ -65,3 +65,12 @@ additions() {
 function remove-from-path() {
     export PATH=$(echo -n "$PATH" | awk -v RS=: -v ORS=: '$0 != "'$1'"' | sed 's/:$//')
 }
+
+# Search for, preview, and open man pages
+fman() {
+    man -k . |
+        fzf -q "$1" --prompt='Man> ' --preview $'echo {} | tr -d \'()\' | awk \'{printf "%s ", $2} {print $1}\' | xargs -r man | col -bx | bat -l man -p --color always' |
+        tr -d '()' |
+        awk '{printf "%s ", $2} {print $1}' |
+        xargs -r man
+}
