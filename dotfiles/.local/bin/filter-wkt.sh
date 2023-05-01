f7517cd48c269ae5e54b0bfa6c191d1b1b43bc6b
Austin Gill <Notgnoshi@gmail.com>
2023-05-01 11:04:35 -0500

    Add better CLI argument parsing to filter-wkt.sh

--- dotfiles/.local/bin/filter-wkt.sh
+++ dotfiles/.local/bin/filter-wkt.sh
@@ -1,9 +1,52 @@
 #!/bin/bash
+# shellcheck disable=SC2155
 set -o errexit
 set -o pipefail
 set -o nounset
 set -o noclobber
 
-INPUT="${1:--}"
+export RED=$(tput setaf 1)
+export RESET=$(tput sgr0)
 
-grep -Poi '(LINESTRING|POINT|POLYGON)\s*Z?\s*\(.*?\)+' "$INPUT"
+error() {
+    echo "${RED}[ERROR]:${RESET} $*" >&2
+}
+
+usage() {
+    echo "Usage: $0 [--help] [--input]"
+    echo
+    echo "Filter out WKT geometries from the given input"
+    echo
+    echo "  --help, -h      Show this help and exit"
+    echo "  --input, -i     Input file to filter. Defaults to stdin"
+}
+
+main() {
+    local input="-"
+
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+        --help | -h)
+            usage
+            exit 0
+            ;;
+        --input | -i)
+            input="$2"
+            shift
+            ;;
+        *)
+            error "Unexpected option: $1"
+            usage >&2
+            exit 1
+            ;;
+        esac
+        shift
+    done
+
+    # TODO: MULTI-geometry variants, GEOMETRYCOLLECTION
+    # TODO: Consider using a real WKT parser, because regex isn't sufficient to match the right
+    # opening/closing parentheses.
+    grep -Poi '(LINESTRING|POINT|POLYGON)\s*Z?\s*\(.*?\)+' "$input"
+}
+
+main "$@"
